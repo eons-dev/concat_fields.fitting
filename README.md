0b5c71f1058a2caf5eabd3505c7cf345eb61f686
eons <eons@eons.dev>
2022-09-04 17:57:20 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# fitting_concat_fields
-Join multiple fields together into a single output
+# Concatenate Fields Fitting
+
+Combine multiple fields through [Pipeadapter](https://github.com/infrastructure-tech/lib_pipeadapter)
+
+## Inputs
+
+### Required
+* `fields` - a dictionary of fields and their contents
+* `match` - the regex to match
+
+### Optional
+* `joiner` (';') - added between matches
+* `field_name` (field_`match`) - what to call the output
---
+++ inc/concat_fields.py
@@ -0,0 +1,28 @@
+import os
+import logging
+import re
+from pipeadapter import Fitting
+from pipeadapter import OtherFittingError
+
+class concat_fields(Fitting):
+    def __init__(this, name="Concat Fields"):
+        super().__init__(name)
+
+        this.requiredKWArgs.append("fields")
+        this.requiredKWArgs.append("match")
+
+        this.optionalKWArgs["joiner"] = ";"
+        this.optionalKWArgs["field_name"] = None
+
+    # Required Fitting method. See that class for details.
+    def Run(this):
+        if (this.field_name is None):
+            this.field_name = f"field_{this.match}"
+        
+        this.output[this.field_name] = ""
+        for field, contents in this.fields.items():
+            if (re.search(this.match, field) is not None):
+                logging.debug(f"Found match for {this.match} in {field}; will add {contents} to {this.field_name}.")
+                if (len(this.output[this.field_name])):
+                    this.output[this.field_name] += this.joiner
+                this.output[this.field_name] += contents
